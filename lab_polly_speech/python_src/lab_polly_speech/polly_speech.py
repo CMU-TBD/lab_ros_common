1b698386ddb38294aa3f43368f9940452eb249e9
Zhi Tan <zhi.tan@ri.cmu.edu>
2017-11-16 14:10:54 -0500

    Implemented wait function in python module

--- lab_polly_speech/python_src/lab_polly_speech/polly_speech.py
+++ lab_polly_speech/python_src/lab_polly_speech/polly_speech.py
@@ -1,5 +1,6 @@
 
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from lab_common.msg import(
     speakGoal,
     speakAction
@@ -12,6 +13,16 @@ class PollySpeech(object):
         self._polly_client.wait_for_server()
 
     def speak(self, text, block=True):
+        """
+        Command the robot to speak the given text.
+
+        Parameters
+        ----------
+        text : str
+            The text that the robot will speak
+        block : bool, optional
+            Whether this call is a blocking call.
+        """
         goal = speakGoal()
         goal.text = text
         if block:
@@ -19,6 +30,18 @@ class PollySpeech(object):
         else:
             self._polly_client.send_goal(goal)
 
-    def wait(self):
-        pass
-        #self._polly_client
+    def wait(self, duration=None):
+        """
+        Wait for the speech to finish. Note, sometimes the last few seconds of the speech will still be playing when it ends
+        
+        Parameters
+        ----------
+        duration : rospy.Duration
+            Ros's implementation of Duration
+
+        """
+        if self._polly_client.gh:
+            if duration is not None:
+                result = self._polly_client.wait_for_result(duration)
+            else:
+                result = self._polly_client.wait_for_result()
