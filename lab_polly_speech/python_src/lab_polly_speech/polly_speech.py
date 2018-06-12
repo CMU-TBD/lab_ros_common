1d9e128f6735ad94ab9c3740dce4c4cfa7ac3631
jmc359 <joe.connolly@yale.edu>
2018-06-12 08:47:20 -0400

    better organization

--- lab_polly_speech/python_src/lab_polly_speech/polly_speech.py
+++ lab_polly_speech/python_src/lab_polly_speech/polly_speech.py
@@ -2,32 +2,39 @@
 import actionlib
 from actionlib_msgs.msg import GoalStatus
 from lab_common.msg import(
-    speakGoal,
-    speakAction
+    polly_speechGoal,
+    polly_speechAction
 )
 
 class PollySpeech(object):
 
     def __init__(self):
-        self._polly_client = actionlib.SimpleActionClient("lab_polly_speech/speak", speakAction)
+        self._polly_client = actionlib.SimpleActionClient("lab_polly_speech/speak", polly_speechAction)
         self._polly_client.wait_for_server()
 
-    def speak(self, text, block=True, cancel=False):
+    def speak(self, text, block=True, cancel=False, voice_id="Joanna", pitch="20%"):
         """
         Command the robot to speak the given text.
 
         Parameters
         ----------
-        text : str
+        text : string, required
             The text that the robot will speak
+        voice_id : string, required
+            Name of desired voice from AWS Polly Speech
+        pitch : string, required 
+            Desired pitch of voice as a percentage (ex: "20%")
         block : bool, optional
             Whether this call is a blocking call.
         cancel : bool, optional
             Whether to cancel all the current speech request to say this instead of 
             queue it
         """
-        goal = speakGoal()
+        goal = polly_speechGoal()
         goal.text = text
+        goal.voice_id = voice_id
+        goal.pitch = pitch
+
         if cancel:
             self._polly_client.cancel_all_goals()
         if block:
